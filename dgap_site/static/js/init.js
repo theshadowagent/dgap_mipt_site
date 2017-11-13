e5da6a13104982089f55075c8d4b2f5953119a59
Shad <shadowagent1st@gmail.com>
2017-11-13 21:01:37 +0300

    Initial commit

---
+++ dgap_site/static/js/init.js
@@ -0,0 +1,10 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav({
+      edge: 'right'
+    });
+    $('.parallax').parallax();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
